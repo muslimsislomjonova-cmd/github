a2b2ae51314b948a01b93a12664147b15c42714e
muslimsislomjonova-cmd <muslimsislomjonova@gmail.com>
2026-01-30 17:43:55 +0500

    nmadr

--- index.js
+++ index.js
@@ -2,33 +2,32 @@ const input = document.querySelector("#username");
 const btn = document.querySelector("#btn");
 const result = document.querySelector("#result");
 
-
 const TOKEN = "ghp_YOURTOKENHERE";
 
-
 btn.addEventListener("click", () => {
+  const username = input.value;
 
-    const username = input.value;
-fetch(`https://api.github.com/users/${username}`)
+  fetch(`https://api.github.com/users/${username}`, {
+    headers: {
+      Authorization: `token ${TOKEN}`
+    }
+  })
     .then(res => {
       if (!res.ok) {
-        throw new Error("user toplmadi");
+        throw new Error("User topilmadi");
       }
       return res.json();
     })
- .then(function (data) {
-  result.innerHTML =
-    '<img src="' + data.avatar_url + '" width="120">' +
-    '<h2>' + (data.name ? data.name : 'No name') + '</h2>' +
-    '<p>@' + data.login + '</p>' +
-    '<p>Followers: ' + data.followers + '</p>' +
-    '<p>Repos: ' + data.public_repos + '</p>' +
-    '<a href="' + data.html_url + '" target="_blank">GitHub profile</a>';
-})  
-
-  .catch(err => {
-      result.innerHTML = "bunday foydalanuvchi yoq";
+    .then(data => {
+      result.innerHTML =
+        `<img src="${data.avatar_url}" width="120">
+         <h2>${data.name || "No name"}</h2>
+         <p>@${data.login}</p>
+         <p>Followers: ${data.followers}</p>
+         <p>Repos: ${data.public_repos}</p>
+         <a href="${data.html_url}" target="_blank">GitHub profile</a>`;
+    })
+    .catch(() => {
+      result.innerHTML = "user not found";
     });
 });
-
-   
